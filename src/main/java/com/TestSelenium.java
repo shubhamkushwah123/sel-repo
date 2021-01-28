339f35f9b5f49bd17b410ca90ce3e6a6bc255ac0
Shubham Singh Kushwah <shubhamkushwah123@gmail.com>
2021-01-28 16:31:43 +0530

    commiting selenium test cases

---
+++ src/main/java/com/TestSelenium.java
@@ -0,0 +1,36 @@
+package com;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TestSelenium {
+
+	@Test
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println("hello maven");
+		System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver webDriver = new ChromeDriver(chromeOptions);
+		
+		System.out.println("Hi, Welcome to the Edureka test Cases");
+		webDriver.get("http://ec2-18-224-17-80.us-east-2.compute.amazonaws.com:8888/addressbook/");
+		
+		webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		webDriver.findElement(By.className("v-button")).click();
+		webDriver.findElement(By.id("gwt-uid-5")).sendKeys("Shubham Singh");
+		webDriver.findElement(By.id("gwt-uid-7")).sendKeys("Kushwah");
+		webDriver.findElement(By.id("gwt-uid-9")).sendKeys("9999999999");
+		webDriver.findElement(By.id("gwt-uid-11")).sendKeys("xyz@gmail.com");
+		webDriver.findElement(By.id("gwt-uid-13")).sendKeys("10/10/1999");
+		webDriver.findElement(By.className("v-button-primary")).click();
+		Thread.sleep(5000);
+		webDriver.close();
+		System.out.println("Test Executedß");
+	}
+
+}
